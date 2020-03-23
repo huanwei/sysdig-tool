41b76692723b15b7cf137b2eecc1b73a8259e650
huanwei <huan@harmonycloud.cn>
2020-03-23 15:46:30 +0800

    add working directory
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- sysdig/sysdig-start.sh
+++ sysdig/sysdig-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name sysdig --privileged -v /var/run/docker.sock:/host/var/run/docker.sock -v /dev:/host/dev -v /proc:/host/proc:ro -v /boot:/host/boot:ro -v /lib/modules:/host/lib/modules:ro -v /usr:/host/usr:ro huanwei/sysdig:0.26.4
+docker run -d --name sysdig --privileged -v /var/run/docker.sock:/host/var/run/docker.sock -v /dev:/host/dev -v /proc:/host/proc:ro -v /boot:/host/boot:ro -v /lib/modules:/host/lib/modules:ro -v /usr:/host/usr:ro -v /tmp:/tmp huanwei/sysdig:0.26.4
